aa42fa34bccc0a92d6b8c24e86c30b8aac988d4a
freetimezp <kraftukrnet@ukr.net>
2024-08-05 21:43:16 +0300

    09. js-portfolio-108. Hero animation.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,13 +1,35 @@
+"use client";
+import { useRef } from "react";
+import { motion, useMotionValueEvent, useScroll, useTransform } from "framer-motion";
 import Button from "@/components/Button";
 import starsBg from "@/assets/stars.png";
 
 export const Hero = () => {
+  const sectionRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['start end', 'end start'],
+  });
+
+  const backgroundPositionY = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
   return (
-    <section
+    <motion.section
+      ref={sectionRef}
       className="h-[492px] md:h-[800px] flex items-center overflow-hidden relative
         [mask-image:linear-gradient(to_bottom,transparent,black_10%,black_90%,transparent)]"
       style={{
-        backgroundImage: `url(${starsBg.src})`
+        backgroundImage: `url(${starsBg.src})`,
+        backgroundPositionY
+      }}
+      animate={{
+        backgroundPositionX: starsBg.width,
+      }}
+      transition={{
+        duration: 120,
+        repeat: Infinity,
+        ease: "linear",
       }}
     >
       <div className="absolute inset-0 
@@ -19,8 +41,22 @@ export const Hero = () => {
         shadow-[-20px_-20px_50px_rgba(255,255,255,0.5),-20px_-20px_80px_rgba(255,255,255,0.1),0_0_50px_rgb(140,69,255)]">
       </div>
 
-      <div className="absolute h-[344px] md:h-[580px] w-[344px] md:w-[580px] border border-white rounded-full top-1/2 left-1/2 
-        -translate-x-1/2 -translate-y-1/2 opacity-20">
+      <motion.div
+        className="absolute h-[344px] md:h-[580px] w-[344px] md:w-[580px] border 
+        border-white rounded-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-20"
+        style={{
+          translateX: "-50%",
+          translateY: "-50%",
+        }}
+        animate={{
+          rotate: '1turn',
+        }}
+        transition={{
+          duration: 60,
+          repeat: Infinity,
+          ease: "linear",
+        }}
+      >
         <div className="absolute h-2 w-2 top-1/2 left-0 bg-white rounded-full -translate-x-1/2
           -translate-y-1/2"></div>
         <div className="absolute h-2 w-2 top-0 left-1/2 bg-white rounded-full -translate-x-1/2
@@ -29,18 +65,46 @@ export const Hero = () => {
           -translate-y-1/2 inline-flex items-center justify-center">
           <div className="w-2 h-2 bg-white rounded-full"></div>
         </div>
-      </div>
+      </motion.div>
 
-      <div className="absolute h-[444px] md:h-[780px] w-[444px] md:w-[780px] rounded-full border 
-        border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed"></div>
+      <motion.div
+        className="absolute h-[444px] md:h-[780px] w-[444px] md:w-[780px] rounded-full border 
+        border-white/20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 border-dashed"
+        style={{
+          translateX: "-50%",
+          translateY: "-50%",
+        }}
+        animate={{
+          rotate: '-1turn',
+        }}
+        transition={{
+          duration: 60,
+          repeat: Infinity,
+          ease: "linear",
+        }}
+      ></motion.div>
 
-      <div className="absolute h-[544px] md:h-[980px] w-[544px] md:w-[980px] rounded-full border 
-        border-white top-1/2 opacity-20 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <motion.div
+        className="absolute h-[544px] md:h-[980px] w-[544px] md:w-[980px] rounded-full border 
+        border-white top-1/2 opacity-20 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        style={{
+          translateX: "-50%",
+          translateY: "-50%",
+        }}
+        animate={{
+          rotate: '1turn',
+        }}
+        transition={{
+          duration: 90,
+          repeat: Infinity,
+          ease: "linear",
+        }}
+      >
         <div className="absolute h-2 w-2 top-1/2 left-0 bg-white rounded-full -translate-x-1/2
           -translate-y-1/2"></div>
         <div className="absolute h-2 w-2 top-1/2 left-full bg-white rounded-full -translate-x-1/2
           -translate-y-1/2"></div>
-      </div>
+      </motion.div>
 
       <div className="container relative mt-16">
         <h1 className="text-8xl md:text-[168px] md:leading-none font-semibold tracking-tighter bg-white
@@ -58,6 +122,6 @@ export const Hero = () => {
           <Button text="Learn More" />
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
