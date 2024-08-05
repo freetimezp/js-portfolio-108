2e1f4a18f1fdb204337ce40d2b87ea7893c1e565
freetimezp <kraftukrnet@ukr.net>
2024-08-05 22:08:03 +0300

    10. js-portfolio-108. LogoTicker animation.

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,4 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
+"use client";
+import { motion } from "framer-motion";
+
 import acmeLogo from "@/assets/logo-acme.png";
 import epexLogo from "@/assets/logo-apex.png";
 import celestialLogo from "@/assets/logo-celestial.png";
@@ -17,13 +20,39 @@ export const LogoTicker = () => {
             </h2>
           </div>
 
-          <div className="flex-1 overflow-hidden
+          <div className="flex flex-1 overflow-hidden
             [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-            <div className="flex flex-none gap-14">
-              {[acmeLogo, epexLogo, celestialLogo, quentomLogo, pulseLogo, echoLogo].map((logo, index) => (
+            <motion.div
+              className="flex flex-none gap-14 pr-14 -translate-x-1/2"
+              initial={{
+                translateX: "-50%",
+              }}
+              animate={{
+                translateX: "0",
+              }}
+              transition={{
+                repeat: Infinity,
+                duration: 30,
+                ease: 'linear',
+              }}
+            >
+              {[
+                acmeLogo,
+                epexLogo,
+                celestialLogo,
+                quentomLogo,
+                pulseLogo,
+                echoLogo,
+                acmeLogo,
+                epexLogo,
+                celestialLogo,
+                quentomLogo,
+                pulseLogo,
+                echoLogo
+              ].map((logo, index) => (
                 <img src={logo.src} alt="logo" key={index} className="h-6 w-auto" />
               ))}
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
